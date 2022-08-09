a6e2cffd556ab459127b3d0b76d260e6044488a7
TihimVecherom <98401044+TihimVecherom@users.noreply.github.com>
2022-08-09 13:23:28 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4069,32 +4069,6 @@
         initSliders();
     }));
     let addWindowScrollEvent = false;
-    function headerScroll() {
-        addWindowScrollEvent = true;
-        const header = document.querySelector("header.header");
-        const headerShow = header.hasAttribute("data-scroll-show");
-        const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
-        const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
-        let scrollDirection = 0;
-        let timer;
-        document.addEventListener("windowScroll", (function(e) {
-            const scrollTop = window.scrollY;
-            clearTimeout(timer);
-            if (scrollTop >= startPoint) {
-                !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
-                if (headerShow) {
-                    if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
-                    timer = setTimeout((() => {
-                        !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
-                    }), headerShowTimer);
-                }
-            } else {
-                header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
-                if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
-            }
-            scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
-        }));
-    }
     setTimeout((() => {
         if (addWindowScrollEvent) {
             let windowScroll = new Event("windowScroll");
@@ -4205,8 +4179,18 @@
         menuBurger.classList.toggle("_icon-active");
         if (menuBody) menuBody.classList.toggle("_body-active");
     }));
+    function onEntry(entry) {
+        entry.forEach((change => {
+            if (change.isIntersecting) change.target.classList.add("element-show");
+        }));
+    }
+    let options = {
+        threshold: [ .5 ]
+    };
+    let observer = new IntersectionObserver(onEntry, options);
+    let script_elements = document.querySelectorAll(".element-animation");
+    for (let elm of script_elements) observer.observe(elm);
     window["FLS"] = true;
     isWebp();
     menuInit();
-    headerScroll();
 })();
